2ff00bae3fdea7a6ab8d28be83234b00fb9a82cd
AVWORLD\jim1852 <jwestman@esri.com>
2018-09-07 12:04:56 -0500

    Properly coded TierII report generator Javascript

--- viewer/js/gis/dijit/TierII.js
+++ viewer/js/gis/dijit/TierII.js
@@ -8,15 +8,22 @@ define([
   'dojo/_base/array',
   'dojo/dom-construct',
   'dojo/topic',
+  'dojo/Deferred',
+  'dojo/promise/all',
 
-  'dojo/text!./TierII/reports/report.html',
-  'dojo/text!./TierII/testdata/data.json',
+  'esri/tasks/QueryTask',
+  'esri/tasks/query',
+  'esri/tasks/RelationshipQuery',
+
+  'dojo/text!./TierII/reports/newreport.html',
+  'dojo/text!./TierII/testdata/newdata.json',
 
   './TierII/libs/Blob',
   './TierII/libs/FileSaver'
 ], function (
   declare, _WidgetBase,
-  lang, json, string, array, domConstruct, topic,
+  lang, json, string, array, domConstruct, topic, Deferred, all,
+  QueryTask, Query, RelationshipQuery,
   reportTemplate, testData
  ) {
 
@@ -25,25 +32,148 @@ define([
     _started: false,
     _topicString: 'generateReport',
 
+    queryUrl: '//gis.dhs.in.gov/arcgis/rest/services/OpenAccess/Tier2relates/MapServer/0',
+    contactsId: 1,
+    reportsId: 0,
+    facilityField: 'FacilityID',
+
     postCreate: function () {
       this.inherited(arguments);
 
-      // subscribe to "generateReport" optional-reportTemplate, optional-jsonFeatures, optional-reportName
+      // subscribe to "generateReport" arg=objectid
       this.own(topic.subscribe(this._topicString, lang.hitch(this, this.generateReport)));
-   },
+    },
 
     startup: function () {
       console.log('TierII Startup');
       this._started = true;
     },
 
-    generateReport: function (report, data, outName) {
-      report = report || reportTemplate;
-      data = data || testData;
-      outName = outName || 'report.html';
-            
-      data = json.parse(data);
-      console.log('Tier-II Features: ' + data.features.length);
+    _querySite: function (objectid) {
+      var def = new Deferred();
+
+      var queryTask = new QueryTask(this.queryUrl);
+
+      var query = new Query();
+      query.objectIds = [objectid];
+      query.outFields = ['*'];
+
+      queryTask.execute(query).then(
+        //success
+        function (featureSet) {
+          if (featureSet && featureSet.features.length && featureSet.features[0]) {
+            def.resolve(featureSet.features[0]);
+          } else {
+            def.resolve(null);
+          }
+        },
+        //error
+        function (err) {
+          console.log(err.message);
+          def.resolve(null);
+        }
+      );
+      return def.promise;
+    },
+
+    _queryContacts: function (objectid) {
+      var def = new Deferred();
+
+      var queryTask = new QueryTask(this.queryUrl);
+
+      var relQuery = new RelationshipQuery();
+      relQuery.objectIds = [objectid];
+      relQuery.outFields = ['*'];
+      relQuery.relationshipId = this.contactsId;
+
+      queryTask.executeRelationshipQuery(relQuery).then(
+        //success
+        function (featureSets) {
+          if (featureSets && featureSets[objectid]) {
+            def.resolve(featureSets[objectid].features); // each has attributes
+          } else {
+            def.resolve(null);
+          }
+        },
+        //error
+        function (err) {
+          console.log(err.message);
+          def.resolve(contacts);
+        }
+      );
+      return def.promise;
+    },
+
+    _queryReports: function (objectid) {
+      var def = new Deferred();
+
+      var queryTask = new QueryTask(this.queryUrl);
+
+      var relQuery = new RelationshipQuery();
+      relQuery.objectIds = [objectid];
+      relQuery.outFields = ['*'];
+      relQuery.relationshipId = this.reportsId;
+
+      queryTask.executeRelationshipQuery(relQuery).then(
+        //success
+        function (featureSets) {
+          if (featureSets && featureSets[objectid]) {
+            def.resolve(featureSets[objectid].features); // each has attributes
+          } else {
+            def.resolve(null);
+          }
+        },
+        //error
+        function (err) {
+          console.log(err.message);
+          def.resolve(null);
+        }
+      );
+      return def.promise;
+    },
+
+    generateReport: function (objectid) {
+      var defs = {
+        'SITE': this._querySite(objectid),
+        'CONTACTS': this._queryContacts(objectid),
+        'REPORTS': this._queryReports(objectid)
+      };
+
+      var siteContactsReports = { 'site': null, 'contacts': null, 'reports': null };
+
+      all(defs).then(
+        //success
+        lang.hitch(this, function (responses) {
+          if (responses && responses.hasOwnProperty('SITE') && responses.SITE) {
+            siteContactsReports.site = responses.SITE;
+          }
+          if (responses && responses.hasOwnProperty('CONTACTS') && responses.CONTACTS) {
+            siteContactsReports.contacts = responses.CONTACTS;
+          }
+          if (responses && responses.hasOwnProperty('REPORTS') && responses.REPORTS) {
+            siteContactsReports.reports = responses.REPORTS;
+          }
+          // format the server-data
+          this._prepareReport(siteContactsReports);
+        }),
+        //error
+        lang.hitch(this, function (err) {
+          console.log(err.message || err);
+          // format a warning report
+          this._prepareReport(siteContactsReports);
+        })
+      );
+    },
+
+    _prepareReport: function (siteContactsReports) {
+      var scr = siteContactsReports || json.parse(testData);
+
+      var report = reportTemplate;
+      var outName = 'Facility-';
+      if (scr && scr.site && scr.site.attributes && scr.site.attributes.hasOwnProperty(this.facilityField)) {
+        outName += scr.site.attributes[this.facilityField] || 'unknown';
+      }
+      outName += '.html';
 
       var lines = report.split('\n');
       var line;
@@ -53,25 +183,45 @@ define([
 
       for (var x in lines) {
         line = lines[x];
+
         // check for chemRows
         if (line.indexOf('**chemRows**') > -1) {
           // get template after flag chemRow
           chemTemplate = line.substring(line.indexOf('**chemRows**') + '**chemRows**'.length);
-          for (var f in data.features) {
+          array.forEach(scr.reports, function (chem) {
             // compute comp_ehs based on IsEHS
-            data.features[f].attributes.comp_ehs = data.features[f].attributes.IsEHS === 0 ? 'Non-EHS' : 'EHS';
-            line = string.substitute(chemTemplate, data.features[f].attributes);
+            chem.attributes.comp_ehs = chem.attributes.Is_EHS === 0 ? 'Non-EHS' : 'EHS';
+
+            for (var key in chem.attributes) {
+              if (chem.attributes.hasOwnProperty(key) && chem.attributes[key] === null) {
+                chem.attributes[key] = 'null';
+              }
+            }
+
+            line = string.substitute(chemTemplate, chem.attributes);
             output += line;
-          }
+          }, this);
+
           // check for regRows
         } else if (line.indexOf('**regRows**') > -1) {
           // get template after flag regRows
-          regTemplate = line.substring(line.indexOf('**regRows**' + '**regRows**'));
+          regTemplate = line.substring(line.indexOf('**regRows**') + '**regRows**'.length);
+          array.forEach(scr.contacts, function (cont) {
+
+            for (var key in cont.attributes) {
+              if (cont.attributes.hasOwnProperty(key) && cont.attributes[key] === null) {
+                cont.attributes[key] = 'null';
+              }
+            }
+
+            line = string.substitute(regTemplate, this._attsToPhone(cont.attributes));
+            output += line;
+          }, this);
 
           // regular row of template row
         } else {
           if (lines[x].indexOf('${') > -1) {
-            line = string.substitute(lines[x], data.features[0].attributes);
+            line = string.substitute(lines[x], scr.site.attributes);
             output += line;
           } else {
             output += line;
@@ -81,6 +231,16 @@ define([
 
       var blob = new Blob([output], { type: "text/html;charset=utf-8" });
       saveAs(blob, outName);
+    },
+
+    _attsToPhone: function (atts) {
+      for (var key in atts) {
+        if (atts.hasOwnProperty(key) && atts[key] && typeof atts[key] === 'number' && atts[key].toString().length === 10) {
+          atts[key] = atts[key].toString();
+          atts[key] = atts[key].substring(0, 3) + '-' + atts[key].substring(3, 6) + '-' + atts[key].substring(6);
+        }
+      }
+      return atts;
     },
 
     noComma: null
